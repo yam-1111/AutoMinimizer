16f69342a436c85604557f1fc7272ce69f8ba14e
yam-1111 <achinay0341ant@student.fatima.edu.ph>
2025-01-22 09:59:29 +0800

    Dev : modifiy the server to default in 5173 port

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
   plugins: [
     react(),
     mode === 'development' &&
